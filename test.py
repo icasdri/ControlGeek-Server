2565a524e3cb3e54c7e51acf17ed614bd825f9b0
icasdri <icasdri@gmail.com>
2016-09-17 17:34:08 -0400

    Basically working Servo with PWM

--- test.py
+++ test.py
@@ -10,35 +10,27 @@ g.setmode(g.BCM)
 class Servo:
     channel = None
     pwm = None
+    freq = 50
 
     def __init__(self, channel):
         self.channel = channel
         g.setup(self.channel, g.OUT)
 
-        freq, _ = Servo.freq_for_angle(0)
-        self.pwm = g.PWM(self.channel, freq)
+        self.pwm = g.PWM(self.channel, self.freq)
 
-    @staticmethod
-    def freq_for_angle(angle):
-        u = 1500 + (angle / 45) * 500
-        d = u / (u + 5)
-        x = 1000000 / u
-        return (x, d)
-
-    def set_angle(self, angle):
-        freq, duty = Servo.freq_for_angle(angle)
-        self.pwm.ChangeFrequency(freq)
-        # self.pwm.ChangeDutyCycle(duty)
+    def set_position(self, pos):  # pos out of 100
+        # pw = (1000 + 1000 * pos / 100) / 1000000
+        # duty = pw * self.freq * 100
+        self.pwm.ChangeDutyCycle(5 + 5 * pos / 100)
 
     def start(self):
-        _, duty = Servo.freq_for_angle(0)
-        self.pwm.start(60)
+        self.pwm.start(5)
 
     def stop(self):
         self.pwm.stop()
 
 
-class LedLight:
+class OnOffLed:
     channel = None
 
     def __init__(self, channel):
@@ -51,8 +43,7 @@ class LedLight:
     def turn_off(self):
         g.output(self.channel, g.LOW)
 
-
-led = LedLight(15)
+led = OnOffLed(15)
 se = Servo(14)
 
 try:
@@ -62,11 +53,11 @@ try:
 
     while True:
         led.turn_off()
-        se.set_angle(45)
+        se.set_position(90)
         sleep(5)
 
         led.turn_on()
-        se.set_angle(0)
+        se.set_position(10)
         sleep(5)
 except KeyboardInterrupt:
     pass  # proceed to exit code
