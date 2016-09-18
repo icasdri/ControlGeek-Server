ac9e7451024f9ce138b1bf30f3f2d03527c5ea9c
icasdri <icasdri@gmail.com>
2016-09-17 20:33:07 -0400

    Working static files and client-side sliders

--- tpi/static/app.js
+++ tpi/static/app.js
@@ -3,3 +3,16 @@ setTimeout(function() {
     v.innerHTML = "Javascript is running";
     document.body.appendChild(v);
 }, 1000);
+
+var servo_slider = document.getElementById('servo_slider');
+var servo_label = document.getElementById('servo_label');
+var led_slider = document.getElementById('led_slider');
+var led_label = document.getElementById('led_label');
+
+servo_slider.oninput = function() {
+    servo_label.innerHTML = 'Servo: ' + servo_slider.value;
+}
+
+led_slider.oninput = function() {
+    led_label.innerHTML = 'LED: ' + led_slider.value;
+}
